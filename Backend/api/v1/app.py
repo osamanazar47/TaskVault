546c0fa52dc1f85a626ddd67212eafe15e8c6e34
osamanazar47 <osamanazar47@gmail.com>
2024-05-23 15:52:48 +0300

    creating app.py for the flask app with registaring the blueprints

--- Backend/api/v1/app.py
+++ Backend/api/v1/app.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 """ Flask application """
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
+from config import Config
+from models import db
+from users import users_bp
+from tasks import tasks_bp
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+app.register_blueprint(users_blueprint, url_prefix='/api')
+app.register_blueprint(tasks_blueprint, url_prefix='/api')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
