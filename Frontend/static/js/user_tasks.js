c4a514c0f4ef4b8bff05d8ed88e35e963b3f8f57
osamanazar47 <osamanazar47@gmail.com>
2024-06-03 15:06:39 +0300

    trying to fix the js of user_tasks

--- Frontend/static/js/user_tasks.js
+++ Frontend/static/js/user_tasks.js
@@ -7,9 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const title = document.getElementById('task_title').value;
         const due_date = document.getElementById('task_due_date').value;
 		const description = document.getElementById('description').value;
-		const urlParams = new URLSearchParams(window.location.search);
-		const userId = urlParams.get('user_id');
+		const userId = window.location.pathname.split('/').pop();
 		const accessToken = localStorage.getItem('accessToken')
+
+		console.log("Form data:", { title, due_date, description });
+        console.log("User ID:", userId);
+        console.log("Access Token:", accessToken);
 
         const response = await fetch(`http://127.0.0.1:5000/api/users/${userId}/tasks`, {
             method: 'POST',
@@ -17,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 'Content-Type': 'application/json',
 				'Authorization': `Bearer ${accessToken}`
             },
-            body: JSON.stringify({ title: title, due_date: due_date, description: description })
+            body: JSON.stringify({ title, due_date, description})
         });
 
         if (response.ok) {
