18c6f6612b22e1e996321555ce553d8946e270f5
osamanazar47 <osamanazar47@gmail.com>
2024-06-04 15:49:36 +0300

    implemented the edit button

--- Frontend/static/js/user_tasks.js
+++ Frontend/static/js/user_tasks.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const new_task_form = document.querySelector('.new_task_form');
 	const userId = new_task_form.dataset.userid;
-	const accessToken = localStorage.getItem('accessToken')
+	const accessToken = localStorage.getItem('accessToken');
 	const tasksList = document.querySelector('.tasks-list');
+    let editMode = false;
+	let editTaskId = null;
+
 
 	new_task_form.addEventListener('submit', async (event) => {
         event.preventDefault();
@@ -11,9 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
         const due_date = document.getElementById('task_due_date').value;
 		const description = document.getElementById('task_description').value;
 
+        let url = `http://127.0.0.1:5000/api/users/${userId}/tasks`;
+        let method = 'POST';
 
-        const response = await fetch(`http://127.0.0.1:5000/api/users/${userId}/tasks`, {
-            method: 'POST',
+        if (editMode) {
+            url = `http://127.0.0.1:5000/api/users/${userId}/tasks/${editTaskId}`;
+            method = 'PATCH';
+        }
+
+        const response = await fetch(url, {
+            method: method,
             headers: {
                 'Content-Type': 'application/json',
 				'Authorization': `Bearer ${accessToken}`
@@ -59,10 +69,28 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
             if (response.ok) {
+                taskItem.classList.add('completed');
 				event.target.innerText = 'Completed';
             } else {
                 alert('Failed to mark task as completed. Please try again.');
             }
-		}
+		} else if (event.target.classList.contains('edit-btn')) {
+            const taskItem = event.target.closest('.task-item');
+            const taskId = taskItem.dataset.taskId;
+            const title = taskItem.querySelector('.task-title').innerText;
+            const due_date = taskItem.querySelector('.task-due-date').innerText;
+            const description = taskItem.querySelector('.task-desc').innerText;
+
+            console.log(`Title: ${title}, Due Date: ${due_date}, Description: ${description}`);
+
+            document.getElementById('task_title').value = title;
+            document.getElementById('task_due_date').value = due_date;
+            document.getElementById('task_description').value = description;
+
+            editMode = true;
+            editTaskId = taskId;
+
+            taskItem.remove();
+        }
 	});
 });
